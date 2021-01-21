def84c3732078a43eaf4b67c8c3bc7281484f68b
robbie-veglahn <61095659+robbie-veglahn@users.noreply.github.com>
2021-01-21 17:11:10 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About Us – Greener</title>
+    <title>Greener – Ethical Investing</title>
     <script>prefinery=window.prefinery||function(){(window.prefinery.q=window.prefinery.q||[]).push(arguments)};</script>
     <script src="https://widget.prefinery.com/widget/v2/zmlfa75o.js" defer></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -13,7 +13,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -36,11 +36,9 @@
             <div class="line3"></div>
         </div> 
     </nav>
+    <div class='prefinery-form-embed'></div>
+
     <script src="app.js"></script>
-    <img src="img/mission-statement.png" class="mission-statement">
-    <p class="about-mission">This will be our mission statement here. Liquae. Cit eictore doluptaecust utem. Lorem Ipsem. Ptaquae doluptatus et inverersped quas di</p>
-
-
 </body>
 
 
